01f70d48d03928cdbb6627f080e69895c110c2ce
Scott W Harden <swharden@gmail.com>
2023-11-24 20:22:29 -0500

    create Version class

---
+++ src/LivePictureViewer/Version.cs
@@ -0,0 +1,7 @@
+﻿namespace LivePictureViewer;
+
+public static class Version
+{
+    public static string VersionString = "2.0";
+    public static string FullName => $"Live Picture Viewer {VersionString}";
+}
